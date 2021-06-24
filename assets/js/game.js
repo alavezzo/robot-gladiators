8e758c1dae99522dbbe995d6305b419cadb899e2
bowdenshot <anthonyelavezzo@gmail.com>
2021-06-23 18:56:01 -0700

    added capability to iterate through each enemy Robot and fight to death

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,43 +9,44 @@ var enemyAttack = 12;
 
 var fight = function(enemyName) {
     // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
     // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     console.log(promptFight);
-    if (promptFight === "FIGHT" || promptFight === "fight") {
-    enemyHealth = enemyHealth - playerAttack;
-    // Log a resulting message to the console so we know that it worked.
-    console.log(playerName + " attacked" + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    
+        if (promptFight === "FIGHT" || promptFight === "fight") {
+            while (enemyHealth > 0) {
+            enemyHealth = enemyHealth - playerAttack;
+            // Log a resulting message to the console so we know that it worked.
+            console.log(playerName + " attacked" + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
-    //Check enemy's health
+            //Check enemy's health
 
-    if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
-    }
-    else {
-        window.alert(enemyName + " still has" + enemyHealth + "health left.")
-    }
+            if (enemyHealth <= 0) {
+                window.alert(enemyName + " has died!");
+            }
+            else {
+                window.alert(enemyName + " still has" + enemyHealth + "health left.")
+            }
 
-    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
-    playerHealth = playerHealth - enemyAttack;
-    // Log a resulting message to the console so we know that it worked.
-    console.log(enemyName + " attacked" + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
-    if (playerHealth <= 0) {
-        window.alert(playerName + " has died!");
-    }
-    else {
-        window.alert(playerName + " still has" + playerHealth + "health left.")
-    }
-}
-
-else if (promptFight === "SKIP" || promptFight === "skip") {
-    var confirmSkip = window.confirm("Are you sure you want to skip?")
-    if (confirmSkip) {
-        window.alert(playerName + "has decided to skip this fight. Goodbye.");
-        playerMoney = playerMoney - 2;
-        console.log(playerMoney)
-    }
+            // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+            playerHealth = playerHealth - enemyAttack;
+            // Log a resulting message to the console so we know that it worked.
+            console.log(enemyName + " attacked" + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+            if (playerHealth <= 0) {
+                window.alert(playerName + " has died!");
+            }
+            else {
+                window.alert(playerName + " still has" + playerHealth + "health left.")
+            }
+        }
+    }     
+    else if (promptFight === "SKIP" || promptFight === "skip") {
+        var confirmSkip = window.confirm("Are you sure you want to skip?")
+        if (confirmSkip) {
+            window.alert(playerName + "has decided to skip this fight. Goodbye.");
+            playerMoney = playerMoney - 2;
+            console.log(playerMoney)
+        }
     else {
         fight(); }
 }
@@ -55,7 +56,9 @@ else if (promptFight === "SKIP" || promptFight === "skip") {
 };
 
 for(var i=0; i < enemyNames.length; i++) {
-    fight(enemyNames[i])
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName)
 }
 
 // Game States 
