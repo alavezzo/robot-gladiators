8dd6d56ff8abb8e35da4e063ef7b29e29b5ffc52
bowdenshot <anthonyelavezzo@gmail.com>
2021-06-24 17:41:18 -0700

    fixed blank/null name response bug

--- assets/js/game.js
+++ assets/js/game.js
@@ -56,7 +56,7 @@ var startGame = function() {
 
     for(var i=0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
-            window.alert("Welcome to Robot Gladiators! Round " + (i+1) + " BEGIN!")
+            window.alert("Welcome to Robot Gladiators! Round " + (i+1) + " BEGIN!");
         var pickedEnemyObj = enemyInfo[i];
         pickedEnemyObj.health = randomNumber(40,60);
         fight(pickedEnemyObj);
@@ -135,8 +135,17 @@ var randomNumber = function(min, max) {
     return value;
 }
 
+var getPlayerName = function() {
+    var name = "";
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?")
+    }
+
+    console.log("Your robot's name is " + name);
+    return name;
+}
 var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
